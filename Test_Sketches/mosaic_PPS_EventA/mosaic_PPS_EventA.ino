6f186db8bf044a6dffefe1662efe095d2d4e08b2
PaulZC <pc235603@gmail.com>
2024-09-17 14:51:17 +0100

    Update mosaic_PPS_EventA.ino

--- Test_Sketches/mosaic_PPS_EventA/mosaic_PPS_EventA.ino
+++ Test_Sketches/mosaic_PPS_EventA/mosaic_PPS_EventA.ino
@@ -15,25 +15,25 @@
   ESP32-WROVER-E Pin Allocations:
   D0  : Boot
   D1  : Serial TX (CH340 RX)
-  D2  : 
+  D2  : Mux A
   D3  : Serial RX (CH340 TX)
   D4  : Serial1 RX - Connected to mosaic-X5 COM4 TX
-  D5  : 
-  D12 : 
+  D5  : N/C
+  D12 : Mux B
   D13 : Serial2 RX - Connected to mosaic-X5 COM1 TX
-  D14 : Serial2 TX - Connected to mosaic-X5 COM1 TX
-  D15 : 
+  D14 : Serial2 TX - Connected to mosaic-X5 COM1 RX
+  D15 : N/C
   D16 : N/A
   D17 : N/A
-  D18 : Mux A
-  D19 : Mux B
+  D18 : GNSS Event - Connected to mosaic-X5 EventB
+  D19 : Charger STAT2
   D21 : I2C SDA
   D22 : I2C SCL
   D23 : mosaic On Off
   D25 : Serial1 TX - Connected to mosaic-X5 COM4 RX
   D26 : DAC (via Mux)
-  D27 : Peripheral Power
-  D32 : Power Control
+  D27 : Peripheral Power Control
+  D32 : Power Sense & Control
   D33 : Fast Off
   A34 : Charger STAT1 - Charge LED
   A35 : Board Detect (2.72V)
@@ -60,15 +60,15 @@ const int lbandRxPin = 4;
 const int lbandTxPin = 25;
 HardwareSerial lbandSerial(1);  // UART1: TX on 25, RX on 4. Connected to mosaic-X5 COM4
 
-const int muxA = 18; // 74HC4052 Multiplexer. Note: this will move to pin 2 on the next PCB rev
-const int muxB = 19; // 74HC4052 Multiplexer. Note: this will move to pin 12 on the next PCB rev
+const int muxA = 2; // 74HC4052 Multiplexer
+const int muxB = 12; // 74HC4052 Multiplexer
 const int SDA_1 = 21;
 const int SCL_1 = 22;
 const int mosaicOnOff = 23; // Drive low for >= 50ms to toggle from on to off and vice versa
 const int muxDAC = 26; // Analog out - via multiplexer
 const int peripheralPower = 27; // Pull high to enable power for the mosaic-X5, microSD, multiplexer and main board Qwiic connector
-const int powerControl = 32; // Default to input pull-up. Low indicates power button is being held. Change to output and drive low for power off
-const int fastOff = 33; // Default to input. Change to output and drive low for fast power off
+const int powerControl = 32; // Default to input pull-up. Low indicates power button is being held
+const int fastOff = 33; // Default to input. Change to output and drive high for fast power off
 const int chargeLED = 34; // Connected to charger STAT1
 const int mosaicReady = 36; // High when module is ready
 const int muxADC = 39; // Analog in - via multiplexer
